9dc7217ec56c7b377ba028421d726f147342fd70
Abdussomad20 <harzkid2018@gmail.com>
2023-01-13 15:14:34 +0100

    techfin commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'grey': '#171717',
+      'button': '#26262B',
+      'small-text': '#6D6D6D',
+      'card': '#1E1F21',
+      'down': '#1E1F21',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
